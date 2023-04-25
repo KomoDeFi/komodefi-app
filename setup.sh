65954ee2b5adbf9c09dfae4f44e836780a3bed95
mylo <imylomylo@gmail.com>
2023-04-25 19:13:58 +0000

    (feature) alphanum weak password enabled

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 ./build-mm2.sh
 ./build-mm2-middleware.sh
-./build-mm2-frontend.sh
+./build-mm2-vuetify3.sh
+cp env.sample .env
+sed -i s/XX_komodefi_XX/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 45 | head -n 1)/g .env
+sed -i s/XX_rpcpassword_XX/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 17 | head -n 1)/g .env
