d57fe6a04b32f859902cbfa452af8f483f4258a7
mylo <imylomylo@gmail.com>
2023-04-25 19:21:56 +0000

    (feature) automatic ip address to env

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@
 cp env.sample .env
 sed -i s/XX_komodefi_XX/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 45 | head -n 1)/g .env
 sed -i s/XX_rpcpassword_XX/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 17 | head -n 1)/g .env
+sed -i s/XX_myip_XX/$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)/g .env
