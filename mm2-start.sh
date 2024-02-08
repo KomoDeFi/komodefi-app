cc26e19c3e1df71c2dd91e28ab75b6b41ba25f08
imylomylo <imylomylo@gmail.com>
2024-02-08 12:20:37 +0000

    (chore) dc env clean up and entrypoint

--- mm2-start.sh
+++ mm2-start.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 cd /usr/local/bin/
-source passphrase.txt
-source userpass.txt
-source netid.txt
+if [ -z "$passphrase" ]
+then
+  source passphrase.txt
+fi
+
+if [ -z "$userpass" ]
+then
+  source userpass.txt
+fi
+
+if [ -z "$netid" ]
+then
+  source netid.txt
+fi
+
 stdbuf -oL mm2 "{\"gui\":\"komodefi\",\"netid\":$netid, \"userhome\":\"/${HOME#"/"}\", \"passphrase\":\"$passphrase\", \"rpc_password\":\"$userpass\", \"rpcip\": \"0.0.0.0\", \"rpc_local_only\": false, \"allow_weak_password\":true, \"dbdir\":\"/usr/local/bin\"}"
 sleep 2
 ps aux | grep mm2
